627f920a727e82137ba6f7d02bc340d17bf4a866
Nikolaus satria <nikolaussatria@gmail.com>
2024-03-18 15:31:39 +0700

    Tugas week4 Fix

---
+++ app/src/main/java/com/example/advweek4160421063/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.advweek4160421063.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.advweek4160421063.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
